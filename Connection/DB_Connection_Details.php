bb76d456857406b0b1d5f095f115723cf5817f0e
Prakhar Sharma <77260373+PS1607@users.noreply.github.com>
2021-05-27 08:56:47 +0530

    Add files via upload

---
+++ Connection/DB_Connection_Details.php
@@ -0,0 +1,9 @@
+<?php
+$server="";
+$username="";
+$password="";
+$db="";
+$t= $_SESSION['logged_user'];
+$conn = new mysqli($server,$username,$password,$db);
+?>
+
---
+++ Connection/DB_Connection_Way2.php
@@ -0,0 +1,8 @@
+<?php
+	$server="";
+	$username="";
+	$password="";
+	$db="";
+
+	$conn = mysqli_connect($server,$username,$password,$db);
+?>
